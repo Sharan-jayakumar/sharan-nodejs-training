bea211ca9c9cc422b5278fd16d0649629ce4aeaf
Sharan Jayakumar <sharan.jayakumar@mallow-tech.com>
2025-09-15 10:35:58 +0530

    with stack implementation

--- week1/simple_practice_programs/problem2.js
+++ week1/simple_practice_programs/problem2.js
@@ -1,25 +1,42 @@
 class LRUCache {
     #count = 0;
-    #obj = {};
-    constructor(cnt = 0) {
-        this.#count = cnt;
+    #cache = {};
+    #stack = [];
+
+    constructor(count = 0) {
+        this.#count = count;
     }
 
     size() {
-        return this.#count;
+        return Object.keys(this.#cache).length;
     }
 
     get(key) {
-        return this.#obj[key] || null;
+        if (!(key in this.#cache)) {
+            return null;
+        }
+        
+        const index = this.#stack.indexOf(key);
+        this.#stack.splice(index, 1);
+        
+        this.#stack.push(key);
+        
+        return this.#cache[key];
     }
 
     put(key, value) {
-        // find and replace
-        this.#obj[key] = value;
-        // add and replace
-        if(Object.keys(this.#obj).length > this.#count) {
-            delete this.#obj[Object.keys(this.#obj)[this.#count - 1]]
+        if (key in this.#cache) {
+            const index = this.#stack.indexOf(key);
+            this.#stack.splice(index, 1);
+        } 
+
+        else if (Object.keys(this.#cache).length >= this.#count) {
+            const leastRecentKey = this.#stack.shift();
+            delete this.#cache[leastRecentKey];
         }
+        
+        this.#cache[key] = value;
+        this.#stack.push(key);
     }
 }
 
@@ -32,7 +49,15 @@ l1.put('d', 10);
 
 console.log(l1.get('a'), l1.get('b'), l1.get('c'), l1.get('d'));
 
-l1.put('b', 3);
-l1.put('c', 10);
+l1.get('b');
+l1.put('e', 5);
 
-console.log(l1.get('b'), l1.get('c'), l1.get('d'));
+console.log(l1.get('a'), l1.get('b'), l1.get('c'), l1.get('d'), l1.get('e'));
+
+const l2 = new LRUCache(2);
+
+l2.put('a', 1);
+l2.put('b', 2);
+l2.get('b');
+l2.put('c', 3);
+console.log(l2.get('a'), l2.get('b'), l2.get('c'));
