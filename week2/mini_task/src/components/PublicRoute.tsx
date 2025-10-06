3e15e25c77683afaa6b9a32d0796392c560d90e2
Sharan Jayakumar <sharan.jayakumar@mallow-tech.com>
2025-10-06 11:54:00 +0530

    proper folder structure made, going to implement mini task from scratch again

--- week2/mini_task/src/components/PublicRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router";
-
-type Props = { children: React.ReactNode };
-
-export default function ProtectedRoute({ children }: Props) {
-  const token = null;
-  return token ? <Navigate to="/dashboard" replace /> :<>{children}</>;
-}
